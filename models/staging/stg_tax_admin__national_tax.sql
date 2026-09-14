29247c7899b6029cfa097e70dcf8e3d160b42739
Kai <yousia39@gmail.com>
2026-09-14 15:59:45 +0900

    feat: 국세 source 기반 staging 모델 추가

---
+++ models/staging/stg_tax_admin__national_tax.sql
@@ -0,0 +1,23 @@
+WITH source AS(
+    SELECT *
+    FROM {{ source('tax_admin_raw', 'national_tax') }}
+),
+
+renamed AS(
+    SELECT
+        `세목별1` AS tax_category_level_1,
+        `세목별2` AS tax_category_level_2,
+        `세목별3` AS tax_category_level_3,
+        `세목별4` AS tax_category_level_4,
+        `세목별5` AS tax_category_level_5,
+        `세목별6` AS tax_category_level_6,
+        `연도` AS tax_year,
+        `지방청` AS regional_tax_office,
+        `지역` AS region,
+        `금액 단위` AS value_type,
+        SAFE_CAST(`세수액` AS FLOAT64) AS tax_value
+    FROM source
+)
+
+SELECT *
+FROM renamed
