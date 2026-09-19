8dfc9f4a1723c1872538d9f442b2e413edaa3061
Kai <yousia39@gmail.com>
2026-09-20 00:39:54 +0900

    refactor: SSoT 준수를 위한 raw data 교체 및 Python 전처리(melt) 로직 갱신
    - 단일 진실 공급원(Single Source of Truth) 원칙 위배 항목(사전 계산된 합계 및 구성비)을 제외하여 raw data 재수집
    - 행렬 구조를 통일화하여 raw data 재수집
    - 갱신된 데이터 구조에 맞추어 preprocess.py 내 pandas melt 함수 전처리 로직 재구성

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -2,54 +2,73 @@ import pandas as pd
 
 
 # 1. 국세 데이터 처리
-
 df_nat = pd.read_csv('raw_data/national_tax_raw.csv', encoding='utf-8', header=[0, 1, 2, 3])
 
 df_nat_melted = df_nat.melt(
-    id_vars=df_nat.columns[:6].tolist(),
-    var_name=['연도', '지방청', '지역', '금액 단위'],
+    id_vars=df_nat.columns[:6].tolist(), 
+    var_name=['연도', '지방청', '지역', '금액 단위'], 
     value_name='세수액'
 )
 
 df_nat_melted.columns = [
-    '세목별1', '세목별2', '세목별3', '세목별4', '세목별5', 
-    '세목별6', '연도', '지방청', '지역', '금액 단위', '세수액'
+    '세목별1',
+    '세목별2',
+    '세목별3',
+    '세목별4',
+    '세목별5',
+    '세목별6',
+    '연도',
+    '지방청',
+    '지역',
+    '금액_단위',
+    '세수액'
 ]
 
-df_nat_melted.to_csv('processed_data/national_tax_processed.csv', encoding='utf-8', index=False)
+df_nat_melted.to_csv('processed_data/national_tax_processed.csv', index=False, encoding='utf-8')
 
 
-# 2. 지방세 데이터 처리 (metro: 특별시, 광역시 / provincial: 도 단위)
+# 2. 지방세 데이터 처리 (metro: 특별시,광역시 / provincial: 도 단위)
 
 target_files = ['local_tax_metro', 'local_tax_provincial']
 
 for file_name in target_files:
     input_path = f'raw_data/{file_name}_raw.csv'
     output_path = f'processed_data/{file_name}_processed.csv'
-    
-    df_loc = pd.read_csv(input_path, encoding='utf-8', header=[0, 1, 2])
-    
-    df_loc_melted = df_loc.melt(
-        id_vars=df_loc.columns[:1].tolist(),
-        var_name=['연도', '분류', '세목'],
+    df = pd.read_csv(input_path, encoding='utf-8', header=[0, 1])
+
+    df_melted = df.melt(
+        id_vars=df.columns[:2].tolist(),
+        var_name=['연도', '지역'],
         value_name='세수액'
     )
-    
-    df_loc_melted.columns = ['자치단체별1', '연도', '분류', '세목', '세수액']
-    
-    df_loc_melted.to_csv(output_path, encoding='utf-8', index=False)
+
+    df_melted.columns = [
+        '세목별1',
+        '세목별2',
+        '연도',
+        '지역',
+        '세수액'
+        ]
+
+    df_melted.to_csv(output_path, index=False, encoding='utf-8')
 
 
-# 3. 인구수 데이터 처리
+# 3. 인구 데이터 처리
 
 df_pop = pd.read_csv('raw_data/population_raw.csv', encoding='utf-8', header=[0, 1])
 
 df_pop_melted = df_pop.melt(
     id_vars=df_pop.columns[:2].tolist(),
-    var_name=['연도', '연령'],
+    var_name=['연도', '지역'],
     value_name='인구수'
-)
+    )
 
-df_pop_melted.columns = ['행정구역별_동읍면', '항목', '연도', '연령', '인구수']
+df_pop_melted.columns = [
+    '항목',
+    '5세별',
+    '연도',
+    '지역',
+    '인구수'
+    ]
 
-df_pop_melted.to_csv('processed_data/population_processed.csv', encoding='utf-8', index=False)
+df_pop_melted.to_csv('processed_data/population_processed.csv', index=False, encoding='utf-8')
