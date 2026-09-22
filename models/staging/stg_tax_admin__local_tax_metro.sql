69f1f1f7b88461bbd446423836964e171829c9aa
Kai <yousia39@gmail.com>
2026-09-22 15:46:32 +0900

    feat: Staging 모델에 지역명 표준화 로직 추가

--- models/staging/stg_tax_admin__local_tax_metro.sql
+++ models/staging/stg_tax_admin__local_tax_metro.sql
@@ -3,15 +3,22 @@ WITH source AS(
     FROM {{ source('tax_admin_raw', 'local_tax_metro') }}
 ),
 
+region_mapping AS(
+    SELECT *
+    FROM {{ ref('region_mapping') }}
+),
+
 renamed AS(
     SELECT
-        SAFE_CAST(`연도` AS INT64) AS tax_year,
-        `지역` AS region,
-        `세목별1` AS tax_category_level_1,
-        `세목별2` AS tax_category_level_2,
-        SAFE_CAST(`세수액` AS INT64) * 1000 AS tax_amount
-    FROM source
-    WHERE `세목별2` != '소계'
+        SAFE_CAST(s.`연도` AS INT64) AS tax_year,
+        r.standard_region AS region,
+        s.`세목별1` AS tax_category_level_1,
+        s.`세목별2` AS tax_category_level_2,
+        SAFE_CAST(s.`세수액` AS INT64) * 1000 AS tax_amount
+    FROM source AS s 
+    LEFT JOIN region_mapping AS r
+        ON s.`지역` = r.raw_region
+    WHERE s.`세목별2` != '소계'
 ),
 
 added_surrogate_key AS(
--- models/staging/stg_tax_admin__population.sql
+++ models/staging/stg_tax_admin__population.sql
@@ -3,14 +3,21 @@ WITH source AS(
     FROM {{ source('tax_admin_raw', 'population') }}
 ),
 
+region_mapping AS(
+    SELECT *
+    FROM {{ ref('region_mapping') }}
+),
+
 renamed AS(
     SELECT
-        SAFE_CAST(`연도` AS INT64) AS population_year,
-        `지역` AS region,
-        `항목` AS gender,
-        `5세별` AS age,
-        SAFE_CAST(`인구수` AS INT64) AS population_count
-    FROM source
+        SAFE_CAST(s.`연도` AS INT64) AS population_year,
+        r.standard_region AS region,
+        s.`항목` AS gender,
+        s.`5세별` AS age,
+        SAFE_CAST(s.`인구수` AS INT64) AS population_count
+    FROM source AS s
+    LEFT JOIN region_mapping AS r
+        ON s.`지역` = r.raw_region
 ),
 
 added_surrogate_key AS(
